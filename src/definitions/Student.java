356e1ee4c206626d4c2d81453874b508a9c312e9
Shivansh Gupta <shivansh.gupta_cs19@gla.ac.in>
2020-08-30 00:25:38 +0530

    correcting user info and adding  fields for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,5 +1,5 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Shivansh Gupta (Shivansh770)
  *  Date: 21/08/20
  *  Time: 3:49 PM
  *  File Name : Student.java
@@ -7,4 +7,10 @@
 package definitions;
 
 public class Student {
+    private String firstName, middleName, lastName;
+    private long universityRollNumber;
+    private int numberOfBooksIssued;
+    private Book[] nameOfBooksIssued;
+
+
 }
